afd3157fe805335a51bb50927b30eab03c0ac64a
AJ <dev@ajabad.com>
2023-05-22 00:50:33 +0800

    Add recipe visualization

---
+++ components/Recipe.vue
@@ -0,0 +1,129 @@
+<template>
+  <section id="recipe" class="rounded-lg bg-slate-950 text-white p-4">
+    <div class="flex gap-4 mb-6">
+      <div>
+        <span class="text-2xl font-medium"> {{ coffeeAmount }}g </span>
+        <span class="text-white text-opacity-80"> coffee </span>
+      </div>
+      <div>
+        <span class="text-2xl font-medium"> {{ finalWaterAmount }}mL </span>
+        <span class="text-white text-opacity-80"> water </span>
+      </div>
+    </div>
+    <div class="flex gap-1 mb-4" id="pours-viz">
+      <div
+        v-for="(pour, i) in allPours"
+        class="pour bg-slate-200 text-black px-4 py-2"
+        :style="`width: calc(${(pour * 100) / finalWaterAmount}%`">
+        <div>
+          <span class="text-xl font-medium">{{ pour }}mL</span>
+          <br />
+          <span class="text-black text-opacity-60">
+            {{
+              allPours
+                .slice(0, i + 1)
+                .reduce((a, b) => a + b, 0)
+                .toFixed(2)
+                .replaceAll(".00", "")
+            }}mL total
+          </span>
+          <br />
+          <span> 10s pour <span v-if="allPours.at(i + 1)" class="text-black text-opacity-60">/ 45s pause</span> </span>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div>
+        <span class="text-2xl font-medium">
+            {{ totalBrewTimeInMinutesAndSeconds.replace("0s", "") }}
+        </span>
+        <span class="text-white text-opacity-80"> total brew time </span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  desiredProfile: number;
+  desiredStrength: number;
+  coffeeAmount: number;
+  waterAmount: number;
+  useRatio: boolean;
+  coffeeRatio: number;
+  waterRatio: number;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  desiredProfile: 50,
+  desiredStrength: 50,
+  useRatio: true,
+  coffeeRatio: 1,
+  waterRatio: 15,
+  coffeeAmount: 15,
+  waterAmount: 225,
+});
+const {
+  desiredProfile,
+  desiredStrength,
+  useRatio,
+  coffeeRatio,
+  waterRatio,
+  coffeeAmount,
+  waterAmount,
+} = toRefs(props);
+
+const finalWaterAmount = computed(() =>
+  parseFloat(
+    (useRatio.value
+      ? (coffeeAmount.value / coffeeRatio.value) * waterRatio.value
+      : waterAmount.value
+    ).toFixed(2)
+  )
+);
+
+const firstPourTotal = computed(() => finalWaterAmount.value * 0.4);
+
+const firstTwoPoursRatio = computed(() => {
+  const firstPourRatio = [0.375, 0.45, 0.5, 0.55, 0.625][desiredProfile.value];
+  return [firstPourRatio, 1 - firstPourRatio];
+});
+
+const firstTwoPoursAmount = computed(() => {
+  const firstPour = firstPourTotal.value * firstTwoPoursRatio.value[0];
+  const secondPour = firstPourTotal.value - firstPour;
+  return [firstPour, secondPour].map((pour) => parseFloat(pour.toFixed(2)));
+});
+
+const lastPourTotal = computed(() => finalWaterAmount.value - firstPourTotal.value);
+
+const lastPoursNumber = computed(() => desiredStrength.value + 1);
+
+const lastPours = computed(() =>
+  new Array(lastPoursNumber.value).fill(lastPourTotal.value / lastPoursNumber.value)
+);
+
+const allPours = computed(() => [...firstTwoPoursAmount.value, ...lastPours.value]);
+
+const totalBrewTime = computed(() => allPours.value.length * 10 + (allPours.value.length - 1) * 45);
+const totalBrewTimeInMinutesAndSeconds = computed(() => {
+  const minutes = Math.floor(totalBrewTime.value / 60);
+  const seconds = totalBrewTime.value - minutes * 60;
+  return `${minutes}m ${seconds}s`;
+});
+</script>
+
+<style lang="postcss" scoped>
+#pours-viz {
+  .pour {
+    /* transition: width 0.3s; */
+    @apply rounded;
+    &:first-of-type {
+      @apply rounded-l-xl;
+    }
+    &:last-of-type {
+      @apply rounded-r-xl;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,137 @@
 <template>
-  <main id="home" class="h-full w-full p-12">
-    <h1 class="text-6xl font-medium mb-2">Simple V60 recipe calculator</h1>
-    <p class="text-2xl text-black text-opacity-60">
-      A simple V60 recipe calculator based on Tetsu Kasuya's 4:6 method.
+  <main id="home" class="h-full w-full p-2 md:p-4 lg:p-8">
+    <h1 class="text-4xl lg:text-6xl font-medium mb-2">Simple V60 recipe calculator</h1>
+    <p class="text-lg lg:text-2xl text-black text-opacity-60">
+      A simple calculator based on Tetsu Kasuya's 4:6 method.
     </p>
-    <hr class="my-8" />
-    <div class="flex">
-      <div class="grow">
-        <label class="block text-lg text-black text-opacity-40 mb-2"> Coffee to water ratio </label>
-        <div class="flex text-4xl font-medium gap-2">
-          <button class="cursor-ns-resize">{{ coffeeRatio }}</button>
-          <span class="text-black text-opacity-60">:</span>
-          <button class="cursor-ns-resize">{{ waterRatio }}</button>
+    <hr class="my-4 lg:my-8" />
+    <section id="parameters" class="mb-12">
+      <NConfigProvider preflight-style-disabled>
+        <div class="mb-4">
+          <span class="font-medium text-black text-opacity-60">PARAMETERS</span>
         </div>
-      </div>
-      <div class="grow">
-        <label class="block text-lg text-black text-opacity-40 mb-2"> Coffee amount </label>
-        <div class="flex text-4xl font-medium gap-2">
-          <input type="number" class="border-b outline-none w-[2ch]" v-model="coffeeAmount">
-          <span class="text-black text-opacity-60">g</span>
+        <div class="grid grid-cols-12 gap-4">
+          <div class="col-span-4 lg:col-span-2 rounded-lg border p-4">
+            <label class="block text-lg text-black text-opacity-80 mb-2 font-medium">
+              Coffee amount
+            </label>
+            <div class="flex text-4xl font-medium gap-2">
+              <input
+                type="number"
+                class="border-b outline-none w-[2ch]"
+                v-model="coffeeAmount"
+                placeholder="15" />
+              <span class="text-black text-opacity-60">g</span>
+            </div>
+          </div>
+          <div class="col-span-8 lg:col-span-2 rounded-lg border p-4">
+            <label class="block text-lg text-black text-opacity-80 font-medium mb-2">
+              Water amount
+            </label>
+            <div class="mb-2 h-">
+              <div class="flex text-4xl font-medium gap-2 mb-2" v-if="useRatio">
+                <input
+                  aria-label="coffee ratio"
+                  placeholder="1"
+                  type="number"
+                  class="border-b outline-none"
+                  :style="`width: ${(coffeeRatio || 1).toString().length}ch`"
+                  v-model="coffeeRatio" />
+                <span class="text-black text-opacity-60">:</span>
+                <input
+                  aria-label="water ratio"
+                  placeholder="15"
+                  type="number"
+                  class="border-b outline-none"
+                  :style="`width: ${(waterRatio || 15).toString().length}ch`"
+                  v-model="waterRatio" />
+              </div>
+              <div class="flex text-4xl font-medium gap-2" v-else>
+                <input
+                  type="number"
+                  class="border-b outline-none w-[3ch]"
+                  v-model="waterAmount"
+                  placeholder="225" />
+                <span class="text-black text-opacity-60">mL</span>
+              </div>
+            </div>
+            <n-checkbox v-model:checked="useRatio">
+              <span class="font-medium text-black text-opacity-50">Use ratio</span>
+            </n-checkbox>
+          </div>
+
+          <div class="col-span-12 md:col-span-6 lg:col-span-4 flex flex-col rounded-lg border p-4">
+            <label class="block text-lg text-black text-opacity-80 mb-2 font-medium">
+              Desired profile
+            </label>
+            <div>
+              <NSlider
+                class="mb-2"
+                v-model:value="desiredProfile"
+                :min="0"
+                :max="4"
+                :marks="{
+                  1: '',
+                  2: '',
+                  3: '',
+                }" />
+              <div class="flex justify-between uppercase text-black text-opacity-60 font-medium">
+                <small>Sweeter</small>
+                <small>Brighter</small>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-12 md:col-span-6 lg:col-span-4 flex flex-col rounded-lg border p-4">
+            <label class="block text-lg text-black text-opacity-80 mb-2 font-medium">
+              Desired strength
+            </label>
+            <div class="grow flex flex-col justify-center">
+              <NSlider
+                v-model:value="desiredStrength"
+                :min="0"
+                :max="3"
+                :marks="{
+                  1: '',
+                  2: '',
+                }" />
+              <div class="flex justify-between uppercase text-black text-opacity-60 font-medium">
+                <small>Mellow</small>
+                <small>Full-bodied</small>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="grow">
-        <label class="block text-lg text-black text-opacity-40 mb-2"> Desired profile </label>
-      </div>
-      <div class="grow">
-        <label class="block text-lg text-black text-opacity-40 mb-2"> Desired strength </label>
-      </div>
+      </NConfigProvider>
+    </section>
+    <div class="mb-2">
+      <span class="font-medium text-black text-opacity-60">RECIPE</span>
     </div>
+    <Recipe
+      :coffee-amount="coffeeAmount || 15"
+      :water-amount="waterAmount || 225"
+      :desired-profile="desiredProfile"
+      :desired-strength="desiredStrength"
+      :coffee-ratio="coffeeRatio || 1"
+      :water-ratio="waterRatio || 15"
+      :use-ratio="useRatio"
+      class="mb-4" />
+    <button type="button" class="border px-4 py-2 rounded-md">
+      <span class="font-medium">Share this recipe</span>
+    </button>
   </main>
 </template>
 
 <script setup lang="ts">
+import { NConfigProvider, NSlider, NCheckbox } from "naive-ui";
+
+const useRatio = ref(true);
+
 const coffeeRatio = ref(1);
 const waterRatio = ref(15);
 
 const coffeeAmount = ref(15);
+const waterAmount = ref(225);
 
+const desiredProfile = ref(2);
+const desiredStrength = ref(0);
 </script>
