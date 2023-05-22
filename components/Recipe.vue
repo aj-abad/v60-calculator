6a5e036ffeb2bc6754a0ea990008b5e1d174552f
AJ <dev@ajabad.com>
2023-05-22 14:07:38 +0800

    Use util to create recipe

--- components/Recipe.vue
+++ components/Recipe.vue
@@ -6,31 +6,27 @@
         <span class="text-white text-opacity-80"> coffee </span>
       </div>
       <div>
-        <span class="text-2xl font-medium"> {{ finalWaterAmount }}mL </span>
+        <span class="text-2xl font-medium"> {{ recipe.finalWaterAmount }}mL </span>
         <span class="text-white text-opacity-80"> water </span>
       </div>
     </div>
     <div class="flex gap-1 mb-4" id="pours-viz">
       <div
-        v-for="(pour, i) in allPours"
+        v-for="(pour, i) in recipe.allPours"
         class="pour bg-slate-200 text-black px-4 py-2"
-        :style="`width: calc(${(pour * 100) / finalWaterAmount}%`">
+        :style="`width: calc(${(pour * 100) / recipe.finalWaterAmount}%`">
         <div>
           <span class="text-xl font-medium">{{ pour }}mL</span>
           <br />
           <span class="text-black text-opacity-60">
-            {{
-              allPours
-                .slice(0, i + 1)
-                .reduce((a, b) => a + b, 0)
-                .toFixed(2)
-                .replaceAll(".00", "")
-            }}mL total
+            {{ recipe.totalWaterPerPour[i].toFixed(2).replaceAll(".00", "") }}mL total
           </span>
           <br />
           <span>
             10s pour
-            <span v-if="allPours.at(i + 1)" class="text-black text-opacity-60">/ 45s pause</span>
+            <span v-if="recipe.allPours.at(i + 1)" class="text-black text-opacity-60"
+              >/ 45s pause</span
+            >
           </span>
         </div>
       </div>
@@ -57,52 +53,14 @@ const props = withDefaults(defineProps<RecipeProps>(), {
   coffeeAmount: 15,
   waterAmount: 225,
 });
-const {
-  desiredProfile,
-  desiredStrength,
-  useRatio,
-  coffeeRatio,
-  waterRatio,
-  coffeeAmount,
-  waterAmount,
-} = toRefs(props);
 
-const finalWaterAmount = computed(() =>
-  parseFloat(
-    (useRatio.value
-      ? (coffeeAmount.value / coffeeRatio.value) * waterRatio.value
-      : waterAmount.value
-    ).toFixed(2)
-  )
-);
+const { coffeeAmount } = toRefs(props);
 
-const firstPourTotal = computed(() => finalWaterAmount.value * 0.4);
+const recipe = computed(() => createRecipe(props));
 
-const firstTwoPoursRatio = computed(() => {
-  const firstPourRatio = [0.375, 0.45, 0.5, 0.55, 0.625][desiredProfile.value];
-  return [firstPourRatio, 1 - firstPourRatio];
-});
-
-const firstTwoPoursAmount = computed(() => {
-  const firstPour = firstPourTotal.value * firstTwoPoursRatio.value[0];
-  const secondPour = firstPourTotal.value - firstPour;
-  return [firstPour, secondPour].map((pour) => parseFloat(pour.toFixed(2)));
-});
-
-const lastPourTotal = computed(() => finalWaterAmount.value - firstPourTotal.value);
-
-const lastPoursNumber = computed(() => desiredStrength.value + 1);
-
-const lastPours = computed(() =>
-  new Array(lastPoursNumber.value).fill(lastPourTotal.value / lastPoursNumber.value)
-);
-
-const allPours = computed(() => [...firstTwoPoursAmount.value, ...lastPours.value]);
-
-const totalBrewTime = computed(() => allPours.value.length * 10 + (allPours.value.length - 1) * 45);
 const totalBrewTimeInMinutesAndSeconds = computed(() => {
-  const minutes = Math.floor(totalBrewTime.value / 60);
-  const seconds = totalBrewTime.value - minutes * 60;
+  const minutes = Math.floor(recipe.value.totalBrewTime / 60);
+  const seconds = recipe.value.totalBrewTime - minutes * 60;
   return `${minutes}m ${seconds}s`;
 });
 </script>
