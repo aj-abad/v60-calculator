06a1a3b6ab58b7898cc3734e04a4e8e3ed9e8932
AJ <dev@ajabad.com>
2023-05-22 10:52:35 +0800

    Some layout changes

--- app.vue
+++ app.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="px-2 md:px-4 lg:px-8 px-2 pt-4 lg:pt-8 select-none">
+    <h1 class="text-4xl lg:text-6xl font-medium mb-2">Simple V60 recipe calculator</h1>
+    <p class="text-lg lg:text-2xl text-black text-opacity-60">
+      A simple calculator based on Tetsu Kasuya's 4:6 method.
+    </p>
+    <hr class="my-4 lg:my-8" />
+  </div>
   <NuxtPage />
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,47 @@
 <template>
-  <main id="home" class="h-full w-full p-2 md:p-4 lg:p-8">
-    <h1 class="text-4xl lg:text-6xl font-medium mb-2">Simple V60 recipe calculator</h1>
-    <p class="text-lg lg:text-2xl text-black text-opacity-60">
-      A simple calculator based on Tetsu Kasuya's 4:6 method.
-    </p>
-    <hr class="my-4 lg:my-8" />
+  <main id="home" class="h-full w-full px-2 md:px-4 lg:px-8">
     <section id="parameters" class="mb-12">
       <NConfigProvider preflight-style-disabled>
         <div class="mb-4">
-          <span class="font-medium text-black text-opacity-60">PARAMETERS</span>
+          <span class="font-medium text-black text-opacity-60 select-none">PARAMETERS</span>
         </div>
         <div class="grid grid-cols-12 gap-4">
-          <div class="col-span-4 lg:col-span-2 rounded-lg border p-4">
-            <label class="block text-lg text-black text-opacity-80 mb-2 font-medium">
+          <div class="col-span-12 md:col-span-4 xl:col-span-2 rounded-lg border p-4">
+            <label
+              for="coffee-amount"
+              class="select-none block text-lg text-black text-opacity-80 mb-2 font-medium">
               Coffee amount
             </label>
             <div class="flex text-4xl font-medium gap-2">
               <input
+                id="coffee-amount"
                 type="number"
-                class="border-b outline-none w-[2ch]"
+                class="border-b outline-none"
+                :style="`width: ${(coffeeAmount || 15).toString().length}ch`"
                 v-model="coffeeAmount"
                 placeholder="15" />
-              <span class="text-black text-opacity-60">g</span>
+              <span class="text-black text-opacity-60 select-none">g</span>
             </div>
           </div>
-          <div class="col-span-8 lg:col-span-2 rounded-lg border p-4">
-            <label class="block text-lg text-black text-opacity-80 font-medium mb-2">
+          <div class="col-span-12 col-span-8 xl:col-span-2 rounded-lg border p-4">
+            <label
+              class="select-none block text-lg text-black text-opacity-80 font-medium mb-2"
+              :for="useRatio ? 'coffee-ratio' : 'water-amount'">
               Water amount
             </label>
             <div class="mb-2 h-">
               <div class="flex text-4xl font-medium gap-2 mb-2" v-if="useRatio">
                 <input
+                  id="coffee-ratio"
                   aria-label="coffee ratio"
                   placeholder="1"
                   type="number"
                   class="border-b outline-none"
                   :style="`width: ${(coffeeRatio || 1).toString().length}ch`"
                   v-model="coffeeRatio" />
-                <span class="text-black text-opacity-60">:</span>
+                <span class="text-black text-opacity-60 select-none" role="presentation">:</span>
                 <input
+                  id="water-ratio"
                   aria-label="water ratio"
                   placeholder="15"
                   type="number"
@@ -48,6 +51,7 @@
               </div>
               <div class="flex text-4xl font-medium gap-2" v-else>
                 <input
+                  id="water-amount" 
                   type="number"
                   class="border-b outline-none w-[3ch]"
                   v-model="waterAmount"
@@ -60,8 +64,8 @@
             </n-checkbox>
           </div>
 
-          <div class="col-span-12 md:col-span-6 lg:col-span-4 flex flex-col rounded-lg border p-4">
-            <label class="block text-lg text-black text-opacity-80 mb-2 font-medium">
+          <div class="col-span-12 md:col-span-6 xl:col-span-4 flex flex-col rounded-lg border p-4">
+            <label class="block text-lg text-black text-opacity-80 mb-2 font-medium select-none">
               Desired profile
             </label>
             <div>
@@ -75,14 +79,14 @@
                   2: '',
                   3: '',
                 }" />
-              <div class="flex justify-between uppercase text-black text-opacity-60 font-medium">
+              <div class="flex justify-between uppercase text-black text-opacity-60 font-medium select-none">
                 <small>Sweeter</small>
                 <small>Brighter</small>
               </div>
             </div>
           </div>
-          <div class="col-span-12 md:col-span-6 lg:col-span-4 flex flex-col rounded-lg border p-4">
-            <label class="block text-lg text-black text-opacity-80 mb-2 font-medium">
+          <div class="col-span-12 md:col-span-6 xl:col-span-4 flex flex-col rounded-lg border p-4">
+            <label class="block text-lg text-black text-opacity-80 mb-2 font-medium select-none">
               Desired strength
             </label>
             <div class="grow flex flex-col justify-center">
@@ -94,7 +98,7 @@
                   1: '',
                   2: '',
                 }" />
-              <div class="flex justify-between uppercase text-black text-opacity-60 font-medium">
+              <div class="flex justify-between uppercase text-black text-opacity-60 font-medium select-none">
                 <small>Mellow</small>
                 <small>Full-bodied</small>
               </div>
